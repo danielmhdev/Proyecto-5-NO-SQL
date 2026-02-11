1335317c40e812fd88e46e68c5b17b3973bee2fa
Daniel Martín Hernández <daniel@MacBook-Air-de-Daniel.local>
2026-02-11 19:34:03 +0100

    creada carpeta routes y archivo movies.routes.js

--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-
-const urlDb = 'mongodb://localhost:27017/proyecto-basico-express-movies'
+//Dado que tengo un mac tengo que cambiar el código para que conecte el localhost por IPv4.
+const urlDb = 'mongodb://127.0.0.1:27017/proyecto-basico-express-movies'
 
 const connect = async () => {
     try {
-        await mongoose.connect(urlDb, { useNewUrlParser: true, useUnifiedTopology: true});
+        await mongoose.connect(urlDb);
         console.log(`Conected with db succesfully`);
     }catch(error) {
-        console.log('Error to connect with db')
+        console.log('Error to connect with db', error.message)
     };
 }
 
